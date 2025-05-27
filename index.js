8147e8206118ced443043c57ac6e32c0ef9f7be5
Mohamamad-Irsyad <124248564+Mohamamad-Irsyad@users.noreply.github.com>
2025-05-27 20:27:49 +0700

    cobalagi

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/products", productRouter);
+app.get("/", (req, res) => {
+  res.send("hellow");
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 2000, () => {
+  console.log(`Server running on port ${process.env.PORT} || 2000`);
 });
